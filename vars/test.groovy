6c439dce791f47a0fbd888cbe3d1dcaf295f736a
YAHIA AHMED <33053353+yoyoraso@users.noreply.github.com>
2023-09-22 11:39:09 +0300

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,24 @@
+def {
+  pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'BUILD'
+            }
+        }
+        stage('TEST') {
+            steps {
+                echo 'TEST'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploy'
+            }
+        }        
+    }
+}
+}
+  
